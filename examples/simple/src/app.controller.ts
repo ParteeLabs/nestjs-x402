12a35a91003d91bfa9277d8371b7d0f86abd870f
0xRichardL <levanthanh.ptit@gmail.com>
2025-11-14 10:57:49 +0700

    docs: GET api with static prices example

--- examples/simple/src/app.controller.ts
+++ examples/simple/src/app.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseInterceptors } from '@nestjs/common';
 import { AppService } from './app.service';
+import { X402ApiOptions, X402Interceptor } from 'nestjs-x402';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
+  @X402ApiOptions({
+    apiPrices: [{ price: '$0.001', network: 'base' }],
+  })
+  @UseInterceptors(X402Interceptor)
   getHello(): string {
     return this.appService.getHello();
   }
--- examples/simple/src/app.module.ts
+++ examples/simple/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { X402Module } from 'nestjs-x402';
+import { facilitator } from '@coinbase/x402';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    X402Module.register({
+      global: true,
+      x402Version: 1,
+      resource: 'https://example.com/my-digital-resource',
+      recipients: [
+        {
+          payTo: '0xYourServiceAddressHere',
+          network: 'base',
+        },
+      ],
+      facilitator,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
